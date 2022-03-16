e7f9e3fe6cd0beb2b53a994b3ac14b8d0abe09df
nabet <nabet@debian-BULLSEYE-live-builder-AMD64>
2022-03-16 18:11:05 -0500

    G-Backtest v 1.0

--- core/settings.py
+++ core/settings.py
@@ -36,7 +36,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
